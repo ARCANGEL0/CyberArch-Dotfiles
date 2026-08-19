3441af7382978e3d72de37ab71d50c521dd00a87
Henry <henry.arcangello@gmail.com>
2026-08-20 00:16:38 +0100

    add theme wallp

--- theme.lua
+++ theme.lua
@@ -14,6 +14,7 @@ hl.exec_cmd("killall -9 waybar mako dunst swaync 2>/dev/null; systemctl --user s
 hl.exec_cmd(os.getenv("HOME") .. "/.local/bin/ags quit -i cyberpunk 2>/dev/null")
 hl.exec_cmd("sleep 1 && " .. cyberpunk .. "/scripts/launch-theme")
 hl.exec_cmd(cyberpunk .. "/scripts/ws pin")
+hl.exec_cmd("awww img " .. os.getenv("HOME") .. "/.config/hypr/themes/cyberpunk/assets/img/lucy_wallpaper.png")
 once(cyberpunk .. "/quickshell/lock.sh")
 
 hl.exec_cmd("mkdir -p " .. os.getenv("HOME") .. "/.config/kitty && ln -sfn " .. cyberpunk .. "/assets/kitty/kitty.conf " .. os.getenv("HOME") .. "/.config/kitty/kitty.conf")
